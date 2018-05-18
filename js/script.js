81fae9aa27cfc9abdad2395b3e84d22fb6cc71de
Ziad Essam Mohamed <meroziko@gmail.com>
2018-05-18 17:45:56 +0200

    now mobile-friendly!

--- js/script.js
+++ js/script.js
@@ -7,10 +7,9 @@ var timer;
 var startTimer;
 var score = 0;
 var scoreSystem = false;
+var matched = 0;
 // Symbols
 var img = [];
-
-var matched = 0;
 var cardsOnTable;
 // Cards shuffling
 function shuffle(array) {
@@ -51,8 +50,9 @@ function addCards(a) {
   cardsOnTable = img.length / 2;
 
 }
+
 function coreGame() {
-    console.log('coreGame() works')
+    // Picking cards
     $( ".table" ).on( "click", "div", function( event ) {
     event.preventDefault();
     $( this ).css('animation-name', 'flip');
@@ -60,8 +60,9 @@ function coreGame() {
         console.log(this)
     picked2.push($(this));    
     picked.push(this.innerHTML);
-        if (scoreSystem == true) {
-                    if (moves < 10) {
+    // Score system...works in CHALLENGING difficulity only
+    if (scoreSystem == true) {
+    if (moves < 12) {
     $('.score').remove();
     for (i = 0; i < 3; i++) {
 $('body').append('<img class = "score" src="img/star.png">');  
@@ -81,7 +82,7 @@ $('body').append('<img class = "score" src="img/star.png">');
     }
 }
         }
-
+    // Match checking
     if (picked2.length > 2) {
         picked2[2].css('animation-name', 'unflip');
         picked.pop();
@@ -90,62 +91,70 @@ $('body').append('<img class = "score" src="img/star.png">');
     }    
     if (picked.length == 2) {
                 moves++;
-            $('.moves').remove();
-            $('header').append('<div style="position: relative; width: 10; height: 0"><h2 class ="moves">moves: ' + moves + '</h2></div>');
-            
-            
+        $('.moves').remove();
+        $('header').append('<div style="position: relative; width: 10; height: 0"><h2 class ="moves">moves: ' + moves + '</h2></div>');
         if (picked[0] == picked[1]){
-            console.log('identical');
             picked = [];
             picked2 = [];
         } else if (picked[0].substring(18, 19) == picked[1].substring(18, 19)) {
-            console.log('match');
-/*            setTimeout(function() {
-            picked2[0].hide();
-            picked2[1].hide();
-            }, 500);*/
             matched++
             picked = [];
             picked2 = [];
         } else {
-          console.log('unmatch');
             setTimeout(function() {
-              picked2[0].css('animation-name', 'unflip');
+            picked2[0].css('animation-name', 'unflip');
             picked2[1].css('animation-name', 'unflip');
             picked2 = [];
             }, 500);
             picked = [];
-
-            
         }
     }
+    // Game ends when all the cards are matched       
     if (cardsOnTable == matched) {
         startTimer = false;
-        swal({
+        // End of game modal
+        if (scoreSystem == true){
+                   swal({
         title:'Awesome!',
-        text:'You have finished the game with score: ' + score + ' and ' + moves + ' move in ' + timer + ' seconds',
+        text:'You have finished the game with ' + score + ' stars and ' + moves + ' moves in ' + timer + ' seconds',
         icon: 'success',    
-         buttons: {
-    cancel: "Play again!",
+        buttons: {
+        cancel: "Play again!",
   },
 })
-.then((value) => {
-  switch (value) {
-
-    default:
-      startGame();
-      
+    .then((value) => {
+    switch (value) {
+        default:
+      startGame();  
+  }
+}); 
+        } else {
+                    swal({
+        title:'Awesome!',
+        text:'You have finished the game with ' + moves + ' moves in ' + timer + ' seconds',
+        icon: 'success',    
+        buttons: {
+        cancel: "Play again!",
+  },
+})
+    .then((value) => {
+    switch (value) {
+        default:
+      startGame();  
   }
 });
+        }
+
 }
-    });
-}                  
+});
+}
 
-
+    // Game starter
 function startGame() {
     timer = 0;
     moves = 0;
     matched = 0;
+    $('.score').remove();
     $('header').children('div').remove();
     $('.table').children('div').remove();
     for (var i=0;i<img.length;i++){
@@ -153,21 +162,21 @@ function startGame() {
     img.splice(i);
     }
     for (i = 1; i <= 8; i++) {
-    img.push('<img src="img/' + i +' - Copy.png" >');
-    img.push('<img src="img/' + i +'.png">');
-}
+    img.push('<img src="img/'+ i +' - Copy.png" >');
+    img.push('<img src="img/'+ i +'.png">');
+} 
+    // Starting Game modal
     swal({
         title:'How to Play',
         text:'1 - Click over any two cards. \n 2 - If they match, they\'ll be keeped.\n 3 - If they don\'t match they\'ll flip over.\n4 - Remember what was on each card and where it was.\n 5 - The game is over when all cards have been matched.\n\nChoose the difficulty below:',
         closeOnClickOutside: false,
          buttons: {
-    cancel: "EASY",
-    CHALLENGING: true,
+        cancel: "EASY",
+        CHALLENGING: true,
   },
 })
 .then((value) => {
   switch (value) {
- 
     case "CHALLENGING":
       addCards('hard');
       startTimer = true;
@@ -178,19 +187,10 @@ function startGame() {
   }
 });
 }
-
-
-
 $(document).ready(function () {    
-
     startGame();
-    // Add cards
-    
-    console.log($('.table').css('width'));
-    // Picking cards
     coreGame();
     // Timer
-    
     setInterval(function() {
         if (startTimer == true) {
        $('.timer').remove();
